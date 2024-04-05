6b98ed48e7da8efb977abc4c66965e3377f5a70f
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2024-04-05 10:09:53 +0200

    Adding properties files handler

---
+++ src/test/java/utils/PropertiesFiles.java
@@ -0,0 +1,33 @@
+package utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesFile {
+
+    private static FileInputStream fis;
+    private static Properties prop = null;
+
+    public static String getProperty(String property) {
+
+        try {
+            fis = new FileInputStream(new File("config.properties"));
+            prop = new Properties();
+            prop.load(fis);
+        } catch(FileNotFoundException e) {
+            System.out.println("Properties File Not Found");
+        } catch(IOException e) {
+            System.out.println("IO Exception while loading Properties File");
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                System.out.println("IO Exception while closing file input stream");
+            }
+        }
+        return prop.getProperty(property).trim();
+    }
+}
