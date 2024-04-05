ecb52c9dcd7874e8f747e789db67e2196a29cad0
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2024-04-05 12:18:01 +0200

    Editing test runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src\\test\\resources\\features",
-        glue = "stepsdefination",
-        plugin = {"pretty","html:target/report/report.html"}
+        features = {"src/test/resources/features"}
+        ,glue = {"stepsdefinition"}
+        ,monochrome = true
+        ,snippets = CucumberOptions.SnippetType.CAMELCASE
+        ,plugin = {"pretty","html:target/report/report.html"}
 )
 public class TestRunner {
 }
